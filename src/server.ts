e903d6a7e1ecfe15fb97b400123e3c042508f260
EricEOL <lopes.eric051@gmail.com>
2021-08-06 18:12:44 -0300

    Criação da rota de categorias

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  return res.json({message: "Hello world"})
-})
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log('Server is running'));
 
