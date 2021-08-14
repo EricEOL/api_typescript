73114293e2e6c729ec62e338362054af9b14bf7b
EricEOL <lopes.eric051@gmail.com>
2021-08-14 12:24:47 -0300

    Utilizando multer para receber arquivos csv por meio das rotas

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+})
 
 categoriesRoutes.post('/', (req, res) => {
   return createCategoryController.handle(req, res);
@@ -13,3 +18,10 @@ categoriesRoutes.get('/', (req, res) => {
 })
 export { categoriesRoutes };
 
+categoriesRoutes.post('/import', upload.single("file"),(req, res) => {
+  
+  const { file } = req;
+  console.log(file);
+  
+  return res.send();
+})
