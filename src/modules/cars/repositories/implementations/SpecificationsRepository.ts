be9e5c8b85d983dd19b02b9a587abc1b6d6cf760
EricEOL <lopes.eric051@gmail.com>
2021-08-15 15:20:16 -0300

    Tratanto dados do csv e armazenando na variável categories

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,7 +1,6 @@
 import { Specification } from "../../model/Specification";
 import { ICreateSpecificationDTO, ISpecificationsRepository } from "../ISpecificationsRepository";
 
-
 class SpecificationsRepository implements ISpecificationsRepository {
   private specifications: Specification[];
 
